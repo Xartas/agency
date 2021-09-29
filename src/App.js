0e4bc08b0903494ab22ceae4f65597eecfc311e6
Sebastian Wieczorek <sebastian.wieczorek1989@gmail.com>
2021-09-29 03:05:06 +0200

    Fix-Wyświetlanie list i divów, ukrywanie RiskMenu

--- src/App.js
+++ src/App.js
@@ -3,82 +3,74 @@ import './App.css';
 
 export default function App() {
 
-  const [company, setCompany] = useState([]);
-  const [product, setProduct] = useState([]);
-
+  const [company, setCompany] = useState("noCompany");
+  const [product, setProduct] = useState(null);
 
   return (
   <>
-    <div className="Pickers">
-      <div className="CompanyMenu">
-        <fieldset>
-          <legend>WYBÓR FIRMY</legend>
-          <label>Wybierz towarzystwo:</label>
+    <div className="Pickers">      
+      <fieldset>
+        <legend>WYBÓR FIRMY</legend>
+        <span>
+          <label><b>Wybierz towarzystwo: </b></label>          
           <label>
-            <span>
-              <select className="CompanyDropList" onChange={(e) => setCompany(e.target.value)}>
-                <option value="noCompany"></option>
-                <option value="pzu" selected="selected">PZU</option>
-                <option value="warta_tuir">WARTA TUiR</option>
-                <option value="warta_tunz">WARTA TUnŻ</option>
-                <option value="interrisk">INTERRISK</option>
-              </select>
-            </span>
+            <select className="CompanyDropList" onChange={(e) => setCompany(e.target.value)}>
+              <option value="noCompany"></option>
+              <option value="pzu">PZU</option>
+              <option value="warta_tuir">WARTA TUiR</option>
+              <option value="warta_tunz">WARTA TUnŻ</option>
+              <option value="interrisk">INTERRISK</option>
+            </select>
           </label>
-        </fieldset>
-      </div>
-
-      <div className="ProductMenu">
-        <fieldset>
-          <legend>WYBÓR PRODUKTU</legend>
-          <label>Wybierz produkt:</label>
+        </span>  
+      </fieldset>    
+      <fieldset>
+        <legend>WYBÓR PRODUKTU</legend>
+        <span>        
+          <label><b>Wybierz produkt:</b></label>        
           <label>
-            {company === "noCompany" && 
-              <span>
-                <select className="ProductDropList" onChange={(e) => setProduct(e.target.value)}>
+            {company === "noCompany"  && 
+              <select className="ProductDropList" onChange={(e) => setProduct(e.target.value)}>
                   <option value="noProduct"></option>
-                </select>
-              </span>}
-            {company === "pzu" && 
-            <span>
+              </select>}
+            {company === "pzu" &&             
               <select className="ProductDropList" onChange={(e) => setProduct(e.target.value)}>
                 <option value="pzu_komunikacja">Komunikacja</option>
                 <option value="pzu_mieszkania">Mieszkania</option>
                 <option value="pzu_inne">Inne</option>
               </select>
-            </span>}
+            }
             {company === "warta_tuir" && 
-            <span>
               <select className="CompanyDropList" onChange={(e) => setProduct(e.target.value)}>
-                <option value="warta_oc_hdi">OC HDI</option>
-                <option value="warta_oc_komfort">OC KOMFORT</option>
-                <option value="warta_assistance">Assistance</option>
+                <option value="warta_komunikacja">Ubezpieczenie komunikacyjne</option>
+                <option value="warta_mieszkania">Ubezpieczenie majątkowe</option>
+                <option value="warta_inne">Inne</option>
               </select>
-            </span>}
+            }
           </label>
-        </fieldset>
-      </div>
+        </span>
+      </fieldset>
     </div>
+    
+    {/*Poniżej znajduje się okno dla wyświetlania ryzyk. Okno powinno być widoczne, dopiero gdy wybrany zostanie produkt */}    
+    {product !== null &&   
     <div className="RiskMenu">
       <fieldset>
         <legend>WYBÓR RYZYK</legend>
-        {product === "warta_oc_komfort" && 
+        {product === "warta_komunikacja" && 
           <label>
-            <p>W cenie pakietu: </p>
-              <input type="checkbox" className="RiskCheckbox" id="hdiOCrisk" value="warta_hdi_OC" checked></input>
-              <label for="hdiOCrisk">OC</label>  
-            <p>Opcje płatne dodatokwo: </p>
+            <h3>W cenie pakietu: </h3>
+              <input type="checkbox" className="RiskCheckbox" id="hdiOCrisk" value="warta_hdi_OC" checked disabled={true}></input>
+              <label htmlFor="hdiOCrisk">OC</label>  
+            <h3>Opcje płatne dodatokwo: </h3>
               <input type="checkbox" className="RiskCheckbox" id="hdiAssStd" value="warta_hdi_assistance_standard"></input>
-              <label for="hdiAssStd">Assistance Standard</label>  
-            
+              <label htmlFor="hdiAssStd">Assistance Standard</label>              
               <input type="checkbox" className="RiskCheckbox" id="hdiNnw5SU" value="warta_hdi_nnw5su"></input>
-              <label for="hdiNnw5SU">NNW - SU 5 000 PLN</label>                
+              <label htmlFor="hdiNnw5SU">NNW - SU 5 000 PLN</label>                
           </label>
         }   
       </fieldset>
-    </div>
-
-
+    </div>}
   </>
   );
 }
